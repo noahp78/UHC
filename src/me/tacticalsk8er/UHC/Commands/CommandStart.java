046ff395657a76e17e44a76eca0827d1be77b0ed
Noah <noahp78@users.noreply.github.com>
2013-12-12 09:13:28 +0100

    Trying to fix the Structure Break bug
    https://github.com/noahp78/UHC/issues/2

--- src/me/tacticalsk8er/UHC/Commands/CommandStart.java
+++ src/me/tacticalsk8er/UHC/Commands/CommandStart.java
@@ -36,6 +36,22 @@ public class CommandStart {
 		UHC.setPVP(true);
 		UHC.setGameRuleValue("naturalRegeneration", "false");
 
+		
+
+		// Teleport players to new world
+		sender.sendMessage("Teleporting Players");
+		StringBuilder players = new StringBuilder();
+		for (Player p : plugin.getServer().getOnlinePlayers()) {
+			Location tele = UHC.getSpawnLocation();
+			p.teleport(tele);
+			players.append(p.getName() + " ");
+			p.sendMessage(ChatColor.GOLD + "[UHC]" + ChatColor.GREEN + "Game Starting soon, prepare for some lag.");
+			plugin.PlayerCount++;
+
+		}
+
+		sender.sendMessage("Spreading Players");
+		plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "spreadplayers 0 0 " + plugin.getConfig().getInt("GameSettings.SpreadDistance") + " " + plugin.getConfig().getInt("GameSettings.Raduis") + " true " + players);
 		// Set up Walls (MAY CAUSE LAG)
 		// TODO Test to see if this causes lag
 		for (int i = 0; i <= Raduis; i++) {
@@ -64,22 +80,6 @@ public class CommandStart {
 				block2.setType(Material.BEDROCK);
 			}
 		}
-
-		// Teleport players to new world
-		sender.sendMessage("Teleporting Players");
-		StringBuilder players = new StringBuilder();
-		for (Player p : plugin.getServer().getOnlinePlayers()) {
-			Location tele = UHC.getSpawnLocation();
-			p.teleport(tele);
-			players.append(p.getName() + " ");
-			p.sendMessage(ChatColor.GOLD + "[UHC]" + ChatColor.GREEN + "Game Starting soon, prepare for some lag.");
-			plugin.PlayerCount++;
-
-		}
-
-		sender.sendMessage("Spreading Players");
-		plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "spreadplayers 0 0 " + plugin.getConfig().getInt("GameSettings.SpreadDistance") + " " + plugin.getConfig().getInt("GameSettings.Raduis") + " true " + players);
-
 		// Timed Start
 		plugin.GameCountDown = true;
 		plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
