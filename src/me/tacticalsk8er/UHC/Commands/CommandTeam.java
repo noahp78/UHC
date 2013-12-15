ba2212364cad4cc75f36207e8cd93c6ab5d792cb
Nick Peterson <tacticalsk8er@gmail.com>
2013-12-15 11:59:27 -0800

    Work on the Team Commands

--- src/me/tacticalsk8er/UHC/Commands/CommandTeam.java
+++ src/me/tacticalsk8er/UHC/Commands/CommandTeam.java
@@ -3,10 +3,38 @@ package me.tacticalsk8er.UHC.Commands;
 import me.tacticalsk8er.UHC.Main;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
 
 public class CommandTeam {
 
 	public CommandTeam(Main plugin, CommandSender sender, String[] args) {
-
+		if (args[1].equalsIgnoreCase("join")) {
+			if (sender instanceof Player) {
+				Player p = (Player) sender;
+				int Team = 0;
+				try {
+					Team = Integer.parseInt(args[2]);
+				} catch (NumberFormatException e) {
+					sender.sendMessage("Invalid Command: Argument 3 needs to be an integer");
+				}
+				if (Team <= 0) {
+					sender.sendMessage("Invalid Command: Team needs to be 1 or higher!");
+				} else {
+					if (Team <= plugin.getConfig().getInt("TeamSettings.MaxTeams")) {
+						sender.sendMessage("Invalid Team Number: You're Team Number is beyond the max allowed Teams");
+					} else {
+						Team playerTeam = plugin.Teams.get(Team);
+						if (playerTeam.getSize() < plugin.getConfig().getInt("TeamSettings.MaxTeamSize")) {
+							playerTeam.addPlayer(p);
+						} else {
+							sender.sendMessage("Team is full try to join another team.");
+						}
+					}
+				}
+			} else {
+				sender.sendMessage("You must be a player to use this command!");
+			}
+		}
 	}
 }
--- src/me/tacticalsk8er/UHC/Main.java
+++ src/me/tacticalsk8er/UHC/Main.java
@@ -1,5 +1,7 @@
 package me.tacticalsk8er.UHC;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Scoreboard;
@@ -8,9 +10,10 @@ import org.bukkit.scoreboard.Team;
 
 public class Main extends JavaPlugin {
 
-	ScoreboardManager manager;
-	Scoreboard Teams;
-	Team Spectators;
+	public ScoreboardManager manager;
+	public Scoreboard Team;
+	public Team Spectators;
+	public List<Team> Teams;
 
 	public static boolean GameStarted = false;
 	public static boolean GameCountDown = false;
@@ -20,8 +23,13 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		getCommand("uhc").setExecutor(new CommandUHC(this));
 		manager = Bukkit.getScoreboardManager();
-		Teams = manager.getNewScoreboard();
-		Spectators = Teams.registerNewTeam("Spectators");
+		Team = manager.getNewScoreboard();
+		Spectators = Team.registerNewTeam("Spectators");
+		for (int i = 1; i <= getConfig().getInt("TeamSettings.MaxTeams"); i++) {
+			Team GameTeam = Team.registerNewTeam("1");
+			Teams.add(GameTeam);
+		}
+
 		getServer().getPluginManager().registerEvents(new GameEvents(this, Spectators), this);
 		makeConfig();
 	}
@@ -32,6 +40,10 @@ public class Main extends JavaPlugin {
 			getConfig().set("GameSettings.Raduis", 1000);
 		if (!(getConfig().contains("GameSettings.SpreadDistance")))
 			getConfig().set("GameSettings.SpreadDistance", 100);
+		if (!(getConfig().contains("TeamSettings.MaxTeams")))
+			getConfig().set("TeamSettings.MaxTeams", 8);
+		if (!(getConfig().contains("TeamSettings.MaxTeamSize")))
+			getConfig().set("TeamSettings.MaxTeamSize", 2);
 		saveConfig();
 	}
 
